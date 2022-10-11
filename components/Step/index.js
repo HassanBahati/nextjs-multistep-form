a7f1cf7d5582e91d5561f48815f5dabd1851149c
HassanBahati <mukisabahati@gmail.com>
2022-10-11 14:51:49 +0300

    add step one to basic form

--- components/Step/index.js
+++ components/Step/index.js
@@ -2,13 +2,14 @@
 import React, { useContext } from "react";
 import { FormContext } from "../../App";
 import { Basic, Success, Workspace } from "../Forms";
+import BasicForm from "../Forms/Basic";
 
 function Step() {
   const { activeStepIndex } = useContext(FormContext);
   let stepContent;
   switch (activeStepIndex) {
     case 0:
-      stepContent = <Basic />;
+      stepContent = <BasicForm />;
       break;
     case 1:
       stepContent = <Workspace />;
