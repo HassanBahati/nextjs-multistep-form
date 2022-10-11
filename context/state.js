03f1265b152b6d619f88ef5033c6b283ec2df8ad
HassanBahati <mukisabahati@gmail.com>
2022-10-11 14:54:53 +0300

    add formdata and set formdata method to context object

--- context/state.js
+++ context/state.js
@@ -4,12 +4,15 @@ const AppContext = createContext();
 
 export const AppWrapper = ({ children }) => {
   const [activeStepIndex, setActiveStepIndex] = useState(0);
+  const [formData, setFormData] = useState({});
   let sharedState = {
     //state here
   };
 
   return (
-    <AppContext.Provider value={{ activeStepIndex, setActiveStepIndex }}>
+    <AppContext.Provider
+      value={{ activeStepIndex, setActiveStepIndex, formData, setFormData }}
+    >
       {children}
     </AppContext.Provider>
   );
