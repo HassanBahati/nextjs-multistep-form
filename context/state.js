68c6db6e63b49ec7a9a6ef90010301f816641902
HassanBahati <mukisabahati@gmail.com>
2022-10-11 12:27:58 +0300

    add intial active stepper index to contextobject

--- context/state.js
+++ context/state.js
@@ -1,17 +1,22 @@
-import { createContext, useContext , useState} from "react";
+import { createContext, useContext, useState } from "react";
 
 const AppContext = createContext();
 
 export const AppWrapper = ({ children }) => {
+  const [activeStepIndex, setActiveStepIndex] = useState(0);
   let sharedState = {
     //state here
   };
 
   return (
-    <AppContext.Provider value={sharedState}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ activeStepIndex, setActiveStepIndex }}>
+      {children}
+    </AppContext.Provider>
   );
 };
+
 
 export const useFormContext = () => {
   return useContext(AppContext);
 };
+
